707d79aa16d2f8669f4d67640036427e3ea936fe
Pakiboy23 <135179989+Pakiboy23@users.noreply.github.com>
2025-09-15 19:44:25 -0400

    update app.tsx

--- Unfluffed Minimalist Brand (5)/src/App.tsx
+++ Unfluffed Minimalist Brand (5)/src/App.tsx
@@ -1,17 +1,17 @@
 import { HashRouter, Route, Routes } from 'react-router'
-import Home from './pages/Home'
-import KitPage from './pages/KitPage'
-import AboutPage from './pages/AboutPage'
-import ContactPage from './pages/ContactPage'
+import home from './pages/home'
+import kitpage from './pages/kitpage'
+import aboutpage from './pages/aboutpage'
+import contactpage from './pages/contactpage'
 
 export default function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/kit/:kitId" element={<KitPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/" element={<home />} />
+        <Route path="/kit/:kitId" element={<kitpage />} />
+        <Route path="/about" element={<aboutpage />} />
+        <Route path="/contact" element={<contactpage />} />
       </Routes>
     </HashRouter>
   )
